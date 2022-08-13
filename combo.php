9619eaad1e2df48a6a511afe73059fe3bf601af9
master-only <109359453+master-only@users.noreply.github.com>
2022-08-13 23:09:25 +0700

    Update combo.php

---
+++ combo.php
@@ -0,0 +1,23 @@
+{"Version":"1.5",
+
+"Url":"https://github.com/master-only/master-only.github.io/raw/main/combo.zip" ,
+
+"Changelog":"  
+
+อัพเดทไฟล์ล่าสุด กด ตกลง
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
+
